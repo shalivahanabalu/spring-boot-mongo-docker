41ed21bad11c2d84585661e0f2695e8a31bac4a4
shalivahanabalu <103567548+shalivahanabalu@users.noreply.github.com>
2022-04-27 22:36:43 +0530

    Update initScript.sh

--- initScript.sh
+++ initScript.sh
@@ -2,4 +2,3 @@
 export MONGO_DB_USERNAME=${MONGO_DB_USERNAME:=`cat ${MONGO_DB_USERNAME_FILE}`}
 export MONGO_DB_PASSWORD=${MONGO_DB_PASSWORD:=`cat ${MONGO_DB_PASSWORD_FILE}`}
 java  -jar ./spring-boot-mongo.jar
-okay
